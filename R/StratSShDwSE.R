f71d9544c4f4e31c809529be3e57a08211931382
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-07-25 15:13:31 -0400

    Update and rename StratSShDSSDwSE.R to StratSShDwSE.R

--- R/StratSShDwSE.R
+++ R/StratSShDwSE.R
@@ -8,7 +8,7 @@
 #'
 #' @export
 
-StratSShDSSDwSE <- function(Coords, Sex, Size, Strata, rep = 1000, print.progress = T, logSSD = F, ...) {
+StratSShDwSE <- function(Coords, Sex, Size, Strata, rep = 1000, print.progress = T, logSSD = F, ...) {
   ## Does SShD and SSD bootstraps
   SShD <- StratSShDBoot(Coords, Sex, Strata, rep = rep, print.progress = print.progress)
   SSD <- StratSSDBoot(Size, Sex, Strata, rep, log = logSSD, ...)
