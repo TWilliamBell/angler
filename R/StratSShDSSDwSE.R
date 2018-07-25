e18d2b458750bac4adff53bd1ce85e4edcff0b33
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-07-25 15:09:14 -0400

    Update StratSShDSSDwSE.R

--- R/StratSShDSSDwSE.R
+++ R/StratSShDSSDwSE.R
@@ -11,7 +11,7 @@
 StratSShDSSDwSE <- function(Coords, Sex, Size, Strata, rep = 1000, print.progress = T, logSSD = F, ...) {
   ## Does SShD and SSD bootstraps
   SShD <- StratSShDBoot(Coords, Sex, Strata, rep = rep, print.progress = print.progress)
-  SSD <- StratSSDBoot(Size, Sex, Strata, rep, log = logSSD...)
+  SSD <- StratSSDBoot(Size, Sex, Strata, rep, log = logSSD, ...)
   Results <- data.frame(Factors = SShD$Factors, SShD.Mean = SShD$SShD.Mean, SShD.SE = SShD$SShD.SE, SSD.Mean = SSD$SSD.Mean, SSD.SE = SSD$SSD.SE)
   Results
 }
