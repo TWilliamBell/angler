ec6c1e4f89a952d276483714e2ea552ada83494a
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-07-25 11:25:29 -0400

    Update SShDFunc.R

--- R/SShDFunc.R
+++ R/SShDFunc.R
@@ -1,6 +1,6 @@
 #' Calculating Sexual Shape Dimorphism
 #'
-#' This function directly calculates SShD.
+#' This function directly calculates SShD.  However it may be a poor estimator for unbalanced designs where there are more of one sex than the other, SShDLM() is more robust.
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
 #' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
 #' @param Zeroed a logical value stating whether you wish to correct the SShD for the variation among individuals regardless of sex (defaults to TRUE)
@@ -21,8 +21,8 @@ SShDFunc <- function(Coords, Sex, Zeroed = TRUE) {
   FData <- Coords[as.character(Sex)=='f', , drop = FALSE]
   MData <- Coords[as.character(Sex)=='m', , drop = FALSE]
 
-  ConsensusF <- apply(FData, 2, mean) ## Average male and female coordinates
-  ConsensusM <- apply(MData, 2, mean)
+  ConsensusF <- colMeans(FData) ## Average male and female coordinates
+  ConsensusM <- colMeans(MData, 2, mean)
 
   DiffFM <- ConsensusF-ConsensusM
 
