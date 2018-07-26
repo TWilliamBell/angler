8b7f8acc851b11f484479a6d376d6ce779c0182f
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-07-26 10:19:07 -0400

    Add files via upload

---
+++ R/StratPSDShape.R
@@ -0,0 +1,20 @@
+#' Calculates the Pooled Standard Deviation of Shape by Group
+#'
+#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD
+#' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#'
+#' @export
+
+StratPSDShape <- function(Strata, Coords, Sex) {
+  Levels <- levels(Strata)
+  n <- nlevels(Strata)
+  Results <- rep(NA_real_, n)
+  for (i in 1:n) {
+    level <- Levels[i]
+    StratCoord <- Coords[Strata == level, , drop = F]
+    StratSex <- Sex[Strata == level]
+    Results[i] <- PooledSDShape(StratCoord, StratSex)
+  }
+  Results
+}
