e6991f78e01e1c41e4bdb0b813f6fe0dd1ac7783
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:38:05 -0400

    Update SShDwSE.R

--- R/SShDwSE.R
+++ R/SShDwSE.R
@@ -1,7 +1,7 @@
 #' SShD and its Standard Error via a Bootstrap
 #'
 #' @param Coords  Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #' @param rep  The number of times that you wish the resampling to be done, defaults to 1000 but more is better except for very small samples.
 #'
 #' @export
