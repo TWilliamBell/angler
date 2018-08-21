a7767c553133b2224240d341b31ec0477cc2a8fa
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 11:48:12 -0400

    Update bootSSD.R

--- R/bootSSD.R
+++ R/bootSSD.R
@@ -1,6 +1,6 @@
 #' @export
 
 bootSSD <- function(Size, Sex, reps = 1000, log = F, ...) { ## Bootstrap for SSD
-  SSDBooted <- boot(Sex, SSDBoot, reps, Size = Size, log = log, ...)
+  SSDBooted <- boot(Sex, SSDBoot, reps, Size = Size, log = log, strata = Sex, ...)
   SSDBooted
 }
