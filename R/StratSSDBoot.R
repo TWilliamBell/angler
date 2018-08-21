67bafa2d1a5559c84422284711c72d4303237f9d
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:44:41 -0400

    Update StratSSDBoot.R

--- R/StratSSDBoot.R
+++ R/StratSSDBoot.R
@@ -1,8 +1,8 @@
 #' Stratified Standard Error calculator for SSD
 #'
 #' @param Size a numerical vector giving the size of each individual as a scalar quantity
-#' @param Sex a character or factor vector recording sex for each individual as 'm' or 'f'
-#' @param Strata a character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SSD
+#' @param Sex a character or factor vector recording sex for each individual as 'm' or 'f', individuals must be in the same order as the Size vector
+#' @param Strata a character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SSD, individuals must be in the same order as the Size vector
 #' @param rep number of times to perform the resampling procedure
 #'
 #' @export
