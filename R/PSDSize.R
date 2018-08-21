72d8eb1036f6bc277927e5eacaa6a17b99b2ded6
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:31:24 -0400

    Update PSDSize.R

--- R/PSDSize.R
+++ R/PSDSize.R
@@ -3,7 +3,7 @@
 #' The standard deviation of size after removing the difference between the sexes and normalizing the individual size to have a mean of 1.
 #'
 #' @param Size A numerical vector giving the size of each individual as a scalar quantity
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the Size vector
 #'
 #' @export
 
