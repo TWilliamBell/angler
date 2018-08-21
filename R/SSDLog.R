4f11badf3128bd1deeb588734c52c329b0b8675b
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:36:00 -0400

    Update SSDLog.R

--- R/SSDLog.R
+++ R/SSDLog.R
@@ -3,7 +3,7 @@
 #' Calculates the log ratio of sexual dimorphism, log(F/M) (adapted with alterations from Smith 1999).  For the SDI (Lovich and Gibbons 1992), use SSDFunc().  Defaults to e as the base for the logarithm.
 #'
 #' @param Size A numerical vector giving the size of each individual as a scalar quantity
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the Size vector
 #' @param Base the base for the logarithm (defaults to e = 2.718...)
 #' @param positiveM Smith 1999 has the log ratio with male biased being positive and female biased being negative (suited to mammalian research), can have the statistic operate in that manner by setting this to TRUE
 #'
