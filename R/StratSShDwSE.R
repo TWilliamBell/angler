a556c7b2e2c3a769e36ace8581d16dfbd7079bc7
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:27:55 -0400

    Update StratSShDwSE.R

--- R/StratSShDwSE.R
+++ R/StratSShDwSE.R
@@ -3,9 +3,9 @@
 #' The SShD mean is the raw Procrustes Distance without adjusting by a permutation test (the default for SShD calculations in this package performs the Permutation Test), and the SShD standard error is the standard error of that estimator.
 #'
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
-#' @param Size A numerical vector giving the size of each individual as a scalar quantity
-#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
+#' @param Size A numerical vector giving the size of each individual as a scalar quantity, individuals should be in the same order as the rows of the Coords
+#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD, individuals should be in the same order as the rows of the Coords
 #' @param rep Number of resamplings you wish to do
 #'
 #' @export
