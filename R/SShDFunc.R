255f365a7b0a9bd9245e3d720aa2b351e8a7bc52
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:37:21 -0400

    Update SShDFunc.R

--- R/SShDFunc.R
+++ R/SShDFunc.R
@@ -2,7 +2,7 @@
 #'
 #' This function directly calculates SShD.  However it may be a poor estimator for unbalanced designs where there are more of one sex than the other, SShDLM() is more robust.
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #' @param Zeroed a logical value stating whether you wish to correct the SShD for the variation among individuals regardless of sex (defaults to TRUE)
 #'
 #' @export
