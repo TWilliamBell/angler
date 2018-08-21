dcae83c70e9e19f8a887352ab31bffc7161685e9
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:46:50 -0400

    Update StratZStat.R

--- R/StratZStat.R
+++ R/StratZStat.R
@@ -1,8 +1,8 @@
 #' Calculate Geomorph's Z-Statistic for Sexual Shape Dimorphism within Groups
 #'
-#' @param Strata 
-#' @param Coords 
-#' @param Sex 
+#' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
+#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD, individuals should be in the same order as the rows of the Coords
 #'
 #' @export
 
