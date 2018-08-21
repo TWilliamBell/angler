24e15851a1ca796b9a0156219a2ea41551360082
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:37:48 -0400

    Update SShDLM.R

--- R/SShDLM.R
+++ R/SShDLM.R
@@ -5,7 +5,7 @@
 #' @export
 #'
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #' @param Zeroed a logical value stating whether you wish to correct the SShD for the variation among individuals regardless of sex (defaults to TRUE)
 
 SShDLM <- function(Coords, Sex, Zeroed = T) {
