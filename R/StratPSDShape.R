fccac2dd7131dd04ba869c12d76196fbec0e4119
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:28:33 -0400

    Update StratPSDShape.R

--- R/StratPSDShape.R
+++ R/StratPSDShape.R
@@ -1,8 +1,8 @@
 #' Calculates the Pooled Standard Deviation of Shape by Group
 #'
-#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD
+#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD, individuals should be in the same order as the rows of the Coords
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
-#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f'
+#' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #'
 #' @export
 
