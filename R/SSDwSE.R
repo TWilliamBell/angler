bc38730e3795d2eaf2260e44fae9af38aedde1e4
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:36:29 -0400

    Update SSDwSE.R

--- R/SSDwSE.R
+++ R/SSDwSE.R
@@ -1,8 +1,8 @@
 #' SSD with Standard Error estimated via a Bootstrap
 #'
 #' @param Size a numerical vector giving the size of each individual as a scalar quantity
-#' @param Sex a character or factor vector recording sex for each individual as 'm' or 'f'
-#' @param Strata a character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SSD
+#' @param Sex a character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the Size vector
+#' @param Strata a character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SSD, individuals should be in the same order as the Size vector
 #' @param rep number of times to perform the resampling procedure
 #'
 #' @export
