fd9ed29be9335a9bbe46c07ca93fa91d43fc507d
TWilliamBell <40838685+TWilliamBell@users.noreply.github.com>
2018-08-21 10:42:13 -0400

    Update StratPSDSize.R

--- R/StratPSDSize.R
+++ R/StratPSDSize.R
@@ -1,7 +1,7 @@
 #' Calculate the Pooled Standard Deviation for Size for each Group
 #'
-#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD, individuals should be in the same order as the Size vector
 #' @param Size A numerical vector giving the size of each individual as a scalar quantity
+#' @param Strata A character or factor vector giving the factor by which we wish to divide individuals into groups before calculating SShD and SSD, individuals should be in the same order as the Size vector
 #' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the Size vector
 #'
 #' @export
