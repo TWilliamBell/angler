ac68431165c200a80fc105d40d950ad844d93e79
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-03-19 13:11:54 -0400

    Progress bar attempt

--- R/PrintProgress.R
+++ R/PrintProgress.R
@@ -1,16 +1,13 @@
 #' @export
 
 PrintProgress <- function(i, n) {
-  if (i%%10 == 1 & i != 11) {
-    cat("Completed the ", i, "st out of ", n, " levels.\n", sep = "")
+  if (i == 1) {
+    cat("|", paste0(rep("=", 60 %/% n), col = ""), sep = "")
   }
-  else if (i%%10 == 2 & i != 12) {
-    cat("Completed the ", i, "nd out of ", n, " levels.\n", sep = "")
-  }
-  else if (i%%10 == 3 & i != 13) {
-    cat("Completed the ", i, "rd out of ", n, " levels.\n", sep = "")
+  else if (i==n) {
+    cat(paste0(rep("=", 60 %/% n), "|", col = ""), sep = "")
   }
   else {
-    cat("Completed the ", i, "th out of ", n, " levels.\n", sep = "")
+    cat(rep("=", 60 %/% n), sep = "")
   }
 }
