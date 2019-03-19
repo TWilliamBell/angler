5e25b8131d57fac56a74974c48b7744c1dcfd663
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-03-19 12:56:38 -0400

    Making name less misleading

--- R/DShape.R
+++ R/DShape.R
@@ -1,13 +1,13 @@
 #' Effect Size of Sexual Shape Dimorphism
 #' 
-#' A common effect size statistic, another way of representing sexual shape dimorphism that is possibly more readily comparable between studies.
+#' A common effect size statistic, another way of representing sexual shape dimorphism that is possibly more readily comparable between studies.  Roughly analogous to the effect size measure D, but don't quote me on that.
 #'
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
 #' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #'
 #' @export
 
-DShape <- function(Coords, Sex) {
+EffectShape <- function(Coords, Sex) {
   SShD <- SShDLM(Coords, Sex, Zeroed = T)
   PSD <- PSDShape(Coords, Sex)
   if (PSD == 0) {
