b56b7794bf560f4f7012032aa0bbbe1ba75c1256
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-03-19 12:58:49 -0400

    Update and rename StratDShape.R to StratEffectShape.R
    Namespace isn't going to be ready until I change that as well.

--- R/StratEffectShape.R
+++ R/StratEffectShape.R
@@ -1,6 +1,6 @@
 #' Effect Size of Sexual Shape Dimorphism by Stratum
 #' 
-#' A potential effect size statistic, another way of representing sexual shape dimorphism that is more readily comparable between studies, calculated by group.
+#' A potential effect size statistic, another way of representing sexual shape dimorphism that is more readily comparable between studies, calculated by group.  Roughly analogous to the D-statistic, but don't quote me on that.
 #' 
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
 #' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
@@ -8,7 +8,7 @@
 #'
 #' @export
 
-StratDShape <- function(Strata, Coords, Sex) {
+StratEffectShape <- function(Strata, Coords, Sex) {
   Results <- StratSShD(Coords, Strata, Sex)$SShD/StratPSDShape(Strata, Coords, Sex)
   Results
 }
