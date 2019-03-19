11ac95d581dda39b527f88465f649bd208dd6dc2
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-03-19 13:03:10 -0400

    Changing naming conventions

--- R/GeomorphStat.R
+++ R/GeomorphStat.R
@@ -1,11 +1,11 @@
-#' Geomorph's Z-statistic for Sexual Shape Dimorphism
+#' Geomorph's Statistic for Sexual Shape Dimorphism
 #'
 #' @param Coords Two-dimensional array of coordinates from geometric morphometric analysis (see two.d.array() in geomorph for correct formatting)
 #' @param Sex A character or factor vector recording sex for each individual as 'm' or 'f', individuals should be in the same order as the rows of the Coords
 #' 
 #' @export
 
-ZStat <- function(Coords, Sex) {
+GeomorphStat <- function(Coords, Sex) {
   Z <- SShDLM(Coords, Sex, Zeroed = T)/ZeroSD(Coords, Sex)
   Z
 }
