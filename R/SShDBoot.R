145d62353cdbd4705a878bc46ace575c628d04d2
William Bell <40838685+TWilliamBell@users.noreply.github.com>
2019-05-04 04:33:38 -0400

    Remove export command to mask function

--- R/SShDBoot.R
+++ R/SShDBoot.R
@@ -1,5 +1,3 @@
-#' @export
-
 SShDBoot <- function(Sex, Indices, Coords) { ## Statistic for SShD bootstrap
   SShD <- SShDLM(Coords = Coords[Indices, , drop = F], Sex = Sex[Indices], Zeroed = F)
   SShD
